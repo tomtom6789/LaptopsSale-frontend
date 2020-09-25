460c4a176440c80a4a3cc811fcfc1bd7fe1911b8
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-09-24 17:49:20 -0700

    Formatted fetchLatops

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 const itemList = document.getElementById('item-list')
+const itemForm = document.getElementById('item-form')
+const itemName = document.getElementById('item-name')
+const itemDescription = document.getElementById('item-description')
+const itemPrice = document.getElementById('item-price')
 
-function fetchItems(){
-fetch('http://localhost:3000/categories')
-.then(res => res.json())
-.then(addItemsToDom)
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchItems()
+    itemForm.addEventListener('submit', handleFormSubmit)
+})
+
+
+function handleFormSubmit(e) {
+    e.preventDefault()
+    debugger;
+    let newObj = {
+        name: itemName.value,
+        description: itemDescription.value, 
+        price: itemPrice.value 
+    }
+
+    let configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        }, 
+        body: Json.stringify
+    }
 }
-function addItemsToDom(response) {
-    response.included.forEach(item => {
-        itemList.innerHTML += `<li id="item-${item.id}">${item.attributes.name}`
+
+
+
+
+
+function fetchItems() {
+    fetch('http://localhost:3000/items')
+    .then(res => res.json())
+    .then(addItems)
+}
+
+function addItems(response) {
+    response.data.forEach( item => {
+        addItemToDom(item)
     })
 }
 
-fetchItems()
 
-
-
+function addItemToDom(item){
+    itemList.innerHTML += `
+    <div id="item-${item.id}>
+    <li>
+    $<span class="price">${item.attributes.price}</span>
+    <strong class="name">${item.attributes.name}</strong>:
+    <span class="description">${item.attributes.description}</span>
+    </li>
+    </div>
+    `
+}
